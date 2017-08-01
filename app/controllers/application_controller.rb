996801e9352eaf01d9466ca6476c4c092744313a
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-08-01 11:41:19 +0200

    Adding all for first views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
+
+  before_action :configure_devise_parameters, if: :devise_controller?
+
+  def configure_devise_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:username, :email, :password, :password_confirmation)
+    end
+  end
+
 end
