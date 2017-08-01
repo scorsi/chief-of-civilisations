b870759bb72cac7dd0ae94318119988fdd4aaec1
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-08-01 12:04:51 +0200

    Fix models

--- app/models/abstract_primary_resource.rb
+++ app/models/abstract_primary_resource.rb
@@ -2,7 +2,7 @@ class AbstractPrimaryResource < ApplicationRecord
 
   belongs_to :main
 
-  def self.find_by_main_name (name)
+  def self.find_by_main_name(name)
     main = Main.find_by_name name
     return nil if main.nil?
     AbstractPrimaryResource.find_by_main_id main.id
--- app/models/main.rb
+++ app/models/main.rb
@@ -1,5 +1,5 @@
 class Main < ApplicationRecord
 
-  validates :main, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9_\-\.]*\z/ }
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-zA-Z0-9_\-\.]*\z/ }
 
 end
