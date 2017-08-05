c662232ae4ee6a0711921d6b4afed8ebf6742626
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-08-05 13:31:53 +0200

    Add method for better select

--- app/models/gather_building.rb
+++ app/models/gather_building.rb
@@ -4,6 +4,9 @@ class GatherBuilding < ApplicationRecord
   belongs_to :building
   belongs_to :resource
 
+  scope :building_name, ->(name) { joins(:building).where('buildings.name' => name) }
+  scope :resource_name, ->(name) { joins(:resource).where('resources.name' => name) }
+
   has_many :gather_building_tiers
 
   alias_attribute :tiers, :gather_building_tiers
